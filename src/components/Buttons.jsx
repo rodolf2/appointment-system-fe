a2996f076e67bc5d7662df4b009a0b1bfd0d3670
Rodolfo Ebajan Jr. <rodolfojrxgt@gmail.com>
2024-12-25 22:18:25 +0800

    Updated the buttons component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,14 +5,14 @@ const Buttons = () => {
 
   return (
     <div className="relative">
-      <div className="flex items-center justify-center flex-col-reverse">
-        <div className="absolute z-10">
+      <div className="flex items-center justify-center flex-col max-w-[1297px] mx-auto">
+        <div className="absolute z-10 text-[27px] flex justify-around w-full">
           {/* Announcement Tab */}
           <button
             onClick={() => setActiveTab("announcement")}
-            className={`border-b-4 bg-[#D2D2D2] transition mx-20 p-4 rounded-sm w-[200px] ${
+            className={`border-b-4 bg-[#D2D2D2] p-5 rounded-sm w-[399px] h-[104px] ${
               activeTab === "announcement"
-                ? "border-orange-500 font-semibold"
+                ? "border-orange-500"
                 : "border-transparent text-gray-600 hover:border-gray-400"
             }`}
           >
@@ -22,9 +22,9 @@ const Buttons = () => {
           {/* How to Appoint Tab */}
           <button
             onClick={() => setActiveTab("howtoappoint")}
-            className={`border-b-4 bg-[#D2D2D2] transition mx-20 p-4 rounded-sm w-[200px] ${
+            className={`border-b-4 bg-[#D2D2D2] p-5 rounded-sm w-[399px] h-[104px] ${
               activeTab === "howtoappoint"
-                ? "border-orange-500 font-semibold"
+                ? "border-orange-500"
                 : "border-transparent text-gray-600 hover:border-gray-400"
             }`}
           >
@@ -34,9 +34,9 @@ const Buttons = () => {
           {/* Guidelines Tab */}
           <button
             onClick={() => setActiveTab("guidelines")}
-            className={`border-b-4 bg-[#D2D2D2] transition mx-20 p-4 rounded-sm w-[200px] ${
+            className={`border-b-4 bg-[#D2D2D2] p-5 rounded-sm w-[399px] h-[104px] ${
               activeTab === "guidelines"
-                ? "border-orange-500 font-semibold"
+                ? "border-orange-500"
                 : "border-transparent text-gray-600 hover:border-gray-400"
             }`}
           >
