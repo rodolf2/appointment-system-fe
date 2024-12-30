6d7176290f0b7ab7bd5df846b718666c23e58f98
Rodolfo Ebajan Jr. <rodolfojrxgt@gmail.com>
2024-12-30 15:04:22 +0800

    Started the Registrar side Students/Alumni

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./pages/About";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 import HowToAppoint from "./pages/HowToAppoint";
-import LandingPage from "./pages/LandingPage";
-import FAQs from "./pages/FAQs";
-import Contact from "./pages/Contact";
-import HomePage from "./pages/HomePage";
-import Home from "./pages/HomePage";
+import Students from "./pages/Students";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <HowToAppoint />
+      <Students />
       <Footer />
     </BrowserRouter>
   );
---
+++ src/pages/Students.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Students = () => {
+  return <div className="max-w-[1440px]"></div>;
+};
+
+export default Students;
