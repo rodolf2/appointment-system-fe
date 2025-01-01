aa941c003124e13cddafe3d0c1cf839ac373e5db
Rodolfo Ebajan Jr. <rodolfojrxgt@gmail.com>
2025-01-01 23:52:12 +0800

    Rename file back to Home.jsx with the correct casing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import HowToAppoint from "./HowToAppoint";
-import Announcement from "./Announcement";
-import Guidelines from "./Guidelines";
 
 const Home = () => {
   return (
     <>
-      <Announcement />
       <HowToAppoint />
-      <Guidelines />
     </>
   );
 };
