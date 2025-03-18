48c71531059ecab50b6f58588a56ccf3cd6f4a88
Rafael Ramos <Ramosraf278@gmail.com>
2025-03-18 13:33:47 +0800

    updated feauture form

--- src/components/appointmentForm/AppSchedule.jsx
+++ src/components/appointmentForm/AppSchedule.jsx
@@ -81,203 +81,205 @@ const AppSchedule = ({ onNext, onBack, currentStep }) => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-[#161f55] relative">
-      {/* Background Layers */}
-      <div className="absolute inset-0 flex flex-col">
-        {/* Top half background */}
-        <div
-          className="h-1/2 bg-cover bg-bottom"
-          style={{
-            backgroundImage:
-              "url('/assets/image/la_verdad_christian_school_apalit_pampanga_cover.jpeg')",
-            opacity: "0.3",
-            backgroundAttachment: "fixed",
-          }}
-        ></div>
-        {/* Bottom half background */}
-        <div className="h-1/2 bg-[#161f55]"></div>
-      </div>
-
-      {/* Main Content */}
-      <div className="relative z-10 flex flex-col justify-center items-center m-8">
-        {/* Title */}
-        <h1 className="font-LatoBold text-[35px] text-white tracking-widest mt-6 mb-4 w-[450px] text-center">
-          REGISTRAR APPOINTMENT
-        </h1>
-        <div className="bg-white rounded-lg shadow-lg p-8 w-[32rem]">
-          <div className="mb-8">
-            <CustomProgressBar currentStep={currentStep} />
+    <>
+      <div className="{ `bg-Primary h-screen relative}">
+        {/* fixed background */}
+        <div className=" fixed inset-0 w-full h-full ">
+          <div
+            className="h-1/2 bg-cover bg-bottom relative"
+            style={{
+              backgroundImage:
+                "url('/assets/image/la_verdad_christian_school_apalit_pampanga_cover.jpeg')",
+              backgroundAttachment: "fixed",
+            }}
+          >
+            <div className="absolute inset-0 bg-Primary opacity-70"></div>
           </div>
-          {/* Instructional Text */}
-          <p className="text-sm text-center font-LatoItalic text-[#161F55] mb-6">
-            Please note that you will receive an email confirmation once your
-            appointment has been scheduled.
-          </p>
-          <p className="text-center font-LatoRegular text-[#000] mb-6">
-            SELECT YOUR PREFERRED DATE AND TIME TO CLAIM YOUR DOCUMENT:
-          </p>
-          {/* Calendar */}
-          <div>
-            {/* Month Navigation */}
-            <div className="flex items-center justify-between mb-4">
-              <h2 className="text-lg font-bold text-[#161f55]">
-                {currentMonth.format("MMMM YYYY")}
-              </h2>
-              <div className="flex space-x-2">
-                {/* Previous Month */}
-                <button
-                  onClick={handlePrevMonth}
-                  className="bg-[#161f55] text-white px-2 text-[20px] font-LatoBold rounded"
-                >
-                  &lt;
-                </button>
-                {/* Next Month */}
-                <button
-                  onClick={handleNextMonth}
-                  className="bg-[#161f55] text-[20px] text-white px-2 font-LatoBold rounded"
-                >
-                  &gt;
-                </button>
-              </div>
-            </div>
-            {/* Weekdays Header */}
-            <div className="grid grid-cols-7 text-center border-b border-gray-300 mb-2">
-              {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day) => (
-                <div key={day} className="text-[#161f55] font-bold">
-                  {day}
-                </div>
-              ))}
-            </div>
-            {/* Calendar Days */}
-            <div className="grid grid-cols-7 text-center">
-              {daysInCalendar.map((day) => {
-                const formattedDate = day.format("YYYY-MM-DD");
-                const booking = bookings[formattedDate];
-
-                return (
-                  <div
-                    key={formattedDate}
-                    className={`p-2 border cursor-pointer ${
-                      day.isSame(currentMonth, "month")
-                        ? "text-[#161f55] font-LatoSemiBold"
-                        : "text-gray-400"
-                    } ${
-                      booking
-                        ? booking.status === "available"
-                          ? "bg-[#3A993D] hover:bg-green-300 focus:bg-green-300 rounded"
-                          : "bg-[#D52121] hover:bg-red-300 rounded focus:bg-red-300 "
-                        : ""
-                    } ${
-                      isSameDay(day, dayjs())
-                        ? "bg-[#161f55] text-white rounded"
-                        : ""
-                    }`}
-                    onClick={() => handleDateClick(day)}
-                  >
-                    {day.date()}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-
-          {/* Time Slots */}
-          <div className="mt-6 min-h-[100px] transition-all duration-300">
-            {selectedDate ? (
-              <>
-                {/* Selected Date and Slots */}
-                <h3 className="text-lg font-bold text-[#161f55]">
-                  {dayjs(selectedDate.date).format("MMMM D, YYYY")}
-                </h3>
-                <p
-                  className={`mt-2 font-bold ${
-                    selectedDate.status === "available"
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }`}
-                >
-                  {selectedDate.status === "available"
-                    ? "Available Slots"
-                    : "Fully Booked"}
-                </p>
-                {selectedDate.status === "available" ? (
-                  <div className="grid grid-cols-2 gap-2 mt-4">
-                    {selectedDate.slots.map((slot) => (
-                      <button
-                        key={slot}
-                        className={`px-2 py-1 rounded ${
-                          selectedTimeSlot === slot
-                            ? "bg-[#161f55] text-white"
-                            : "bg-gray-200 text-black hover:bg-gray-300"
-                        }`}
-                        onClick={() => handleTimeSlotClick(slot)}
-                      >
-                        {slot}
-                      </button>
-                    ))}
-                  </div>
-                ) : null}
-              </>
-            ) : (
-              <p className="text-gray-500 text-center">
-                Select a date to see available slots.
-              </p>
-            )}
-            {/* Error Message */}
-            {errorMessage && (
-              <p className="text-red-500 text-center mt-4">{errorMessage}</p>
-            )}
-          </div>
-
-          {/* Navigation Buttons */}
-          <div className="flex justify-between mt-6">
-            <button
-              onClick={() => onBack(3)} // Go back to step 3
-              className="bg-[#161f55] hover:bg-blue-700 text-white px-4 py-2 rounded-md"
-            >
-              Back
-            </button>
-            <button
-              onClick={handleSubmit}
-              className={`px-4 py-2 rounded-md ${
-                selectedTimeSlot
-                  ? "bg-[#161f55] hover:bg-blue-700 text-[#fff]"
-                  : "bg-[#161f55] hover:bg-blue-700 text-[#fff]"
-              }`}
-            >
-              Submit
-            </button>
-          </div>
+          <div className="h-1/2 bg-[#161f55]"></div>
         </div>
 
-        {/* Confirmation Modal */}
-        {showConfirmation && (
-          <div className="fixed inset-0 bg-[#D2D2D2] bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-96 h-50 flex  justify-center flex-col">
-              <h2 className="text-lg  text-[#161f55] mb-10 text-center">
-                Are you sure you want to submit now?
-              </h2>
-              <div className="flex justify-between">
-                {/* Cancel Submission */}
-                <button
-                  onClick={handleCancelSubmit}
-                  className="bg-[#C9C9C9]  hover:bg-gray-400 text-[#161F55] px-10 py-1 rounded-lg"
-                >
-                  No
-                </button>
-                {/* Confirm Submission */}
-                <button
-                  onClick={handleConfirmSubmit}
-                  className="bg-[#161F55] hover:bg-blue-700 text-white px-10 py-1 rounded-lg"
-                >
-                  Yes
-                </button>
+        {/* Main Content */}
+        <div className="relative flex flex-col justify-center text-center">
+          {/* Title */}
+          <h1 className="font-LatoBold text-[35px] text-Fwhite tracking-widest py-8">
+            REGISTRAR APPOINTMENT
+          </h1>
+          <div className="relative mx-auto bg-white p-5 rounded-lg shadow-md w-full max-w-[50%] text-center ">
+            {/* Progress Bar */}
+            <div className="w-full max-w-[50%] place-self-center mt-4">
+              <CustomProgressBar currentStep={currentStep} />
+            </div>
+            {/* Instructional Text */}
+            <p className="text-[16px] w-[400px] mx-auto  font-LatoItalic text-[#161F55] mt-16 mb-8">
+              Please note that you will receive an email confirmation once your
+              appointment has been scheduled.
+            </p>
+
+            <p className="text-center font-LatoRegular text-[#000] mb-6 w-[500px] mx-auto">
+              SELECT YOUR PREFERRED DATE AND TIME TO CLAIM YOUR DOCUMENT:
+            </p>
+            {/* Calendar */}
+            <div>
+              {/* Month Navigation */}
+              <div className="flex justify-start px-8 gap-4 mb-4">
+                <h2 className="text-lg font-bold text-[#161f55]">
+                  {currentMonth.format("MMMM YYYY")}
+                </h2>
+                <div className="flex space-x-2">
+                  {/* Previous Month */}
+                  <button
+                    onClick={handlePrevMonth}
+                    className="bg-[#161f55] text-white px-2 text-[20px] font-LatoBold rounded"
+                  >
+                    &lt;
+                  </button>
+                  {/* Next Month */}
+                  <button
+                    onClick={handleNextMonth}
+                    className="bg-[#161f55] text-[20px] text-white px-2 font-LatoBold rounded"
+                  >
+                    &gt;
+                  </button>
+                </div>
+              </div>
+              {/* Weekdays Header */}
+              <div className="grid grid-cols-7 text-center  mb-2">
+                {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(
+                  (day) => (
+                    <div key={day} className="text-[#161f55] font-bold">
+                      {day}
+                    </div>
+                  )
+                )}
+              </div>
+              {/* Calendar Days */}
+              <div className="grid grid-cols-7 text-center">
+                {daysInCalendar.map((day) => {
+                  const formattedDate = day.format("YYYY-MM-DD");
+                  const booking = bookings[formattedDate];
+
+                  return (
+                    <div
+                      key={formattedDate}
+                      className={`p-2 border cursor-pointer ${
+                        day.isSame(currentMonth, "month")
+                          ? "text-[#161f55] font-LatoSemiBold"
+                          : "text-gray-400"
+                      } ${
+                        booking
+                          ? booking.status === "available"
+                            ? "bg-[#3A993D] hover:bg-green-300 focus:bg-green-300 rounded"
+                            : "bg-[#D52121] hover:bg-red-300 rounded focus:bg-red-300 "
+                          : ""
+                      } ${
+                        isSameDay(day, dayjs())
+                          ? "bg-[#161f55] text-white rounded"
+                          : ""
+                      }`}
+                      onClick={() => handleDateClick(day)}
+                    >
+                      {day.date()}
+                    </div>
+                  );
+                })}
               </div>
             </div>
+            {/* Time Slots */}
+            <div className="mt-6 min-h-[100px] transition-all duration-300">
+              {selectedDate ? (
+                <>
+                  {/* Selected Date and Slots */}
+                  <h3 className="text-lg font-bold text-[#161f55]">
+                    {dayjs(selectedDate.date).format("MMMM D, YYYY")}
+                  </h3>
+                  <p
+                    className={`mt-2 font-bold ${
+                      selectedDate.status === "available"
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {selectedDate.status === "available"
+                      ? "Available Slots"
+                      : "Fully Booked"}
+                  </p>
+                  {selectedDate.status === "available" ? (
+                    <div className="grid grid-cols-2 gap-2 mt-4">
+                      {selectedDate.slots.map((slot) => (
+                        <button
+                          key={slot}
+                          className={`px-2 py-1 rounded ${
+                            selectedTimeSlot === slot
+                              ? "bg-[#161f55] text-white"
+                              : "bg-gray-200 text-black hover:bg-gray-300"
+                          }`}
+                          onClick={() => handleTimeSlotClick(slot)}
+                        >
+                          {slot}
+                        </button>
+                      ))}
+                    </div>
+                  ) : null}
+                </>
+              ) : (
+                <p className="text-gray-500 text-center">
+                  Select a date to see available slots.
+                </p>
+              )}
+              {/* Error Message */}
+              {errorMessage && (
+                <p className="text-red-500 text-center mt-4">{errorMessage}</p>
+              )}
+            </div>
+            {/* Navigation Buttons */}
+            <div className=" flex justify-end gap-2 mt-6">
+              <button
+                onClick={() => onBack(3)} // Go back to step 3
+                className="bg-[#161f55] hover:bg-blue-700 text-white px-4 py-2 rounded-md"
+              >
+                Back
+              </button>
+              <button
+                onClick={handleSubmit}
+                className={`px-4 py-2 rounded-md ${
+                  selectedTimeSlot
+                    ? "bg-[#161f55] hover:bg-blue-700 text-[#fff]"
+                    : "bg-[#161f55] hover:bg-blue-700 text-[#fff]"
+                }`}
+              >
+                Submit
+              </button>
+            </div>
           </div>
-        )}
+          {/* Confirmation Modal */}
+          {showConfirmation && (
+            <div className="fixed inset-0 bg-[#D2D2D2] bg-opacity-50 flex items-center justify-center">
+              <div className="bg-white p-6 rounded-lg shadow-lg w-96 h-50 flex  justify-center flex-col">
+                <h2 className="text-lg  text-[#161f55] mb-10 text-center">
+                  Are you sure you want to submit now?
+                </h2>
+                <div className="flex justify-end gap-2 mt-4 ">
+                  {/* Cancel Submission */}
+                  <button
+                    onClick={handleCancelSubmit}
+                    className="bg-[#C9C9C9]  hover:bg-gray-400 text-[#161F55] px-10 py-1 rounded-lg"
+                  >
+                    No
+                  </button>
+                  {/* Confirm Submission */}
+                  <button
+                    onClick={handleConfirmSubmit}
+                    className="bg-[#161F55] hover:bg-blue-700 text-white px-10 py-1 rounded-lg"
+                  >
+                    Yes
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/appointmentForm/Attachment.jsx
+++ src/components/appointmentForm/Attachment.jsx
@@ -31,102 +31,106 @@ const Attachment = ({ onNext, onBack, currentStep }) => {
   };
 
   return (
-    <div className="h-screen flex flex-col bg-[#161f55]">
-      {/* Top Half - Image with Text */}
-      <div
-        className="relative h-1/2 bg-cover bg-center flex items-center justify-center"
-        style={{
-          backgroundImage:
-            "url('/assets/image/la_verdad_christian_school_apalit_pampanga_cover.jpeg')",
-          opacity: "0.3",
-        }}
-      ></div>
-
-      {/* Bottom Half - Solid Color */}
-      <div className="h-1/2 relative bg-[#161f55] flex flex-col items-center justify-center">
-        <h2 className="text-white relative bottom-56 font-LatoBold w-[450px] text-center flex justify-center text-[35px] tracking-widest z-10 pt-20">
-          APPLICATION FOR RECORDS
-        </h2>
-        {/* Content Card */}
-        <div className="bottom-44 relative flex flex-col bg-white p-8 rounded-lg shadow-md w-[50%] text-center z-10 items-center">
-          <div className="w-full max-w-[50%] mx-auto mb-5">
-            <CustomProgressBar currentStep={currentStep} />
+    <>
+      <div className="{ `bg-Primary h-screen relative}">
+        {/* fixed background */}
+        <div className=" fixed inset-0 w-full h-full ">
+          <div
+            className="h-1/2 bg-cover bg-bottom relative"
+            style={{
+              backgroundImage:
+                "url('/assets/image/la_verdad_christian_school_apalit_pampanga_cover.jpeg')",
+              backgroundAttachment: "fixed",
+            }}
+          >
+            <div className="absolute inset-0 bg-Primary opacity-70"></div>
           </div>
-          <p className="text-[#161F55] font-LatoItalic mb-4">
-            To proceed with your request, please upload the required
-            requirements. Processing will not begin until all necessary
-            requirements are submitted and approved by the administrator. The
-            status of your request will be updated accordingly.
-          </p>
+          <div className="h-1/2 bg-[#161f55]"></div>
+        </div>
+        <div className="relative flex flex-col justify-center text-center">
+          <h2 className="font-LatoBold text-[35px] text-Fwhite tracking-widest py-8">
+            APPLICATION FOR RECORDS
+          </h2>
+          <div className="relative mx-auto bg-white p-5 rounded-lg shadow-md w-full max-w-[50%]  text-center z-10">
+            {/* Progress Bar */}
+            <div className="w-full max-w-[50%] place-self-center mt-4 ">
+              <CustomProgressBar currentStep={currentStep} />
+            </div>
 
-          <h2 className="text-gray-800 font-bold text-lg mb-4">ATTACHMENT</h2>
-
-          {/* File List */}
-          <div className="mb-4">
-            {files.length > 0 ? (
-              files.map((file, index) => (
-                <div
-                  key={index}
-                  className="flex justify-between items-center bg-gray-100 p-2 rounded-lg shadow-sm mb-2"
-                >
-                  <span className="text-gray-800">{file.name}</span>
-                  <button
-                    className="text-red-600 hover:text-red-800 text-sm"
-                    onClick={() => handleRemoveFile(index)}
+            <p className="text-[#161F55] font-LatoItalic mt-20 mb-8 w-[540px] mx-auto">
+              To proceed with your request, please upload the required
+              requirements. Processing will not begin until all necessary
+              requirements are submitted and approved by the administrator. The
+              status of your request will be updated accordingly.
+            </p>
+            <h2 className="text-gray-800 font-bold text-lg mb-4">ATTACHMENT</h2>
+            {/* File List */}
+            <div className="mb-4 flex flex-col items-center">
+              {files.length > 0 ? (
+                files.map((file, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-between w-[500px] items-center bg-gray-100 p-2 rounded-lg shadow-sm mb-2"
                   >
-                    Remove
-                  </button>
-                </div>
-              ))
-            ) : (
-              <p className="text-gray-500">No files added.</p>
-            )}
-          </div>
+                    <span className="text-gray-800">{file.name}</span>
+                    <button
+                      className="text-red-600 hover:text-red-800 text-sm"
+                      onClick={() => handleRemoveFile(index)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <p className="text-gray-500 mt-2">No files added.</p>
+              )}
+            </div>
 
-          {/* Hidden File Input */}
-          <input
-            type="file"
-            ref={fileInputRef}
-            onChange={handleFileChange}
-            className="my-2 justify-start flex text-[#161f55] border-2 py-2 px-2 w-[380px] rounded-[20px] border-[#161f55]"
-            multiple // Allow multiple file selection
-          />
+            {/* Hidden File Input */}
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              className={`my-2 justify-center shadow-md text-[#161f55] border-2 py-2 px-2 w-[380px] rounded-[20px] 
+    ${error ? "border-red-500" : "border-[#000] border-opacity-40"}`}
+              multiple // Allow multiple file selection
+            />
 
-          {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+            {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
 
-          {/* Add Another File Button */}
-          <div className="pr-[210px]">
-            <button
-              type="button"
-              className="border-2 border-[#161f55] font-LatoBold text-[#000] text-[16px] px-4 py-2 rounded-[20px] w-[170px] mb-4 hover:bg-[#161f55] hover:text-[#fefefe] transition-colors flex justify-start text-start bg-none"
-              onClick={handleAddFile}
-            >
-              + Add Another File
-            </button>
-          </div>
-
-          {/* Navigation Buttons */}
-          <div className="flex justify-evenly w-full">
-            <button
-              className="bg-[#161f55] text-white px-6 py-2 rounded-lg hover:bg-blue-800"
-              onClick={(e) => {
-                e.preventDefault();
-                onBack();
-                Navigate("/appointmentForm?step=3");
-              }}
-            >
-              Back
-            </button>
-            <button
-              className="bg-[#161f55] text-white px-6 py-2 rounded-lg hover:bg-blue-800"
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            {/* Add Another File Button */}
+            <div className="pr-[210px]">
+              <button
+                type="button"
+                className="border-2 shadow-md border-[#425066] border-opacity-40 font-LatoBold text-[#000] text-[16px] px-4 py-2 rounded-[20px] w-[170px] mb-4 hover:bg-[#161f55] hover:text-[#fefefe] transition-colors  justify-center text-start bg-none"
+                onClick={handleAddFile}
+              >
+                + Add Another File
+              </button>
+            </div>
+            {/* Navigation Buttons */}
+            <div className="flex justify-end gap-2 mt-4 ">
+              <button
+                className="bg-[#161f55] text-white px-6 py-2 rounded-lg hover:bg-blue-800"
+                onClick={(e) => {
+                  e.preventDefault();
+                  onBack();
+                  Navigate("/appointmentForm?step=3");
+                }}
+              >
+                Back
+              </button>
+              <button
+                className="bg-[#161f55] text-white px-6 py-2 rounded-lg hover:bg-blue-800"
+                onClick={handleNext}
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
