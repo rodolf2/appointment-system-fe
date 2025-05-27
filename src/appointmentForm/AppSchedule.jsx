cdf0f7225c263eba43e0026c27891531cf8593bf
rodolf2 <rodolfojrxgt@gmail.com>
2025-05-27 23:40:00 +0800

    Done with the students page

--- src/appointmentForm/AppSchedule.jsx
+++ src/appointmentForm/AppSchedule.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import CustomProgressBar from "@/features/appointment/CustomProgressBar";
 import useAppSchedule from "./hooks/useAppSchedule";
 import dayjs from "dayjs";
@@ -123,13 +122,8 @@ const AppSchedule = ({ onNext, onBack, currentStep }) => {
                         <span>{day.date()}</span>
                         {booking?.status === "available" ? (
                           <span className="text-xs font-semibold text-white">
-<<<<<<< HEAD
-                            {booking.schedule.availableSlots} slot
-                            {booking.schedule.availableSlots !== 1 ? "s" : ""}
-=======
                             {/* {booking.schedule.availableSlots} slot */}
                             {/* {booking.schedule.availableSlots !== 1 ? "s" : ""} */}
->>>>>>> 1d3f27aef474547e97b97d0f4c685df50691a8a8
                           </span>
                         ) : booking?.status === "unavailable" &&
                           booking?.reason === "Fully booked" ? (
