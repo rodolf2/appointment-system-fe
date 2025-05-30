48f16f64bf26f2a82dc6c895cf4f846a7e1e9070
rodolf2 <rodolfojrxgt@gmail.com>
2025-05-31 01:45:43 +0800

    Debug

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        target: process.env.REACT_APP_API_URL || 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       }
